87fc2847b020fe78122648bf52228cfbf72266d1
ToxicSDev <72464460+ToxicSDev@users.noreply.github.com>
2023-05-25 03:52:24 +0300

    Implemented editable text field & task list

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Task from "./Task";
+import EditableText from "./EditableText";
+
+const TaskList = ({
+    tasks,
+    onMoveTask,
+    onDeleteTask,
+    onEditTask,
+    onValueClick,
+}) => {
+    return (
+        <ul className="tasks-list">
+            {tasks.map((task) => (
+                <Task id={task.id} key={task.id} onMoveTask={onMoveTask}>
+                    <EditableText
+                        editing={task.editing}
+                        id={task.id}
+                        value={task.text}
+                        onDelete={onDeleteTask}
+                        onEdit={onEditTask}
+                        onValueClick={onValueClick}
+                    />
+                </Task>
+            ))}
+        </ul>
+    );
+};
+
+export default TaskList;
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -2,7 +2,7 @@ const uuidPattern = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
 const validationRegex = /^[A-Za-z0-9]{8}-[A-Za-z0-9]{4}-4[A-Za-z0-9]{3}-[A-Za-z0-9]{4}-[A-Za-z0-9]{12}$/;
 
 function generateUUIDv4() {
-    return uuidPattern.replace(/[xy]/g, function(char) {
+    return uuidPattern.replace(/[xy]/g, function (char) {
         var rand = (Math.random() * 16) | 0;
         return (char === 'x' ? rand : (rand & 0x3) | 0x8).toString(16);
     });
