2c1aecc7a4de51e36bdf66593cc3b866557d217b
ToxicSDev <72464460+ToxicSDev@users.noreply.github.com>
2023-06-05 21:37:20 +0300

    Defined redux action & object types

---
+++ src/redux/types/actionTypes.js
@@ -0,0 +1,9 @@
+export const CREATE_CATEGORY = "CREATE_CATEGORY";
+export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
+export const DELETE_CATEGORY = "DELETE_CATEGORY";
+export const MOVE_CATEGORY = "MOVE_CATEGORY";
+
+export const MOVE_TASK = "MOVE_TASK";
+export const CREATE_TASK = "CREATE_TASK";
+export const UPDATE_TASK = "UPDATE_TASK";
+export const DELETE_TASK = "DELETE_TASK";
