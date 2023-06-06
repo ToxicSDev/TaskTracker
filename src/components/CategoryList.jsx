7125ae8dde4cf3bf243b04f3b1d5f8d1a95e888a
ToxicSDev <72464460+ToxicSDev@users.noreply.github.com>
2023-06-06 20:05:44 +0300

    Implemented CategoryList component

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Category from "../../containers/Category";
+
+const CategoryList = ({ categories, onEditCategory, onDeleteCategory, onMoveCategory }) => {
+    return (
+        <div className="categories">
+            {categories.map((category) => (
+                <Category
+                    key={category.id}
+                    category={category}
+                    onEditCategory={onEditCategory}
+                    onDeleteCategory={onDeleteCategory}
+                    onMoveCategory={onMoveCategory}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default CategoryList;
